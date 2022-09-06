e6f55e5c26e1b1309350da3a58e69fecf2e4f36d
enterkvas <alexanderbugakov66@gmail.com>
2022-09-05 22:20:12 -0300

    The fourth commit: The header block 320 first edit

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@
 // Burger menu
 const nav = document.querySelector('.navigation'),
    iconBurger = document.querySelector('.burger-btn'),
-   navList = document.querySelector('.menu');
+   navList = document.querySelector('.menu'),
+   container = document.querySelector('._container');
 iconBurger.addEventListener("click", function(e) {
    iconBurger.classList.toggle('_active');
    navList.classList.toggle('_active');
+   container.
    window.scrollTo({ top: 0, behavior: 'smooth' });
 });
